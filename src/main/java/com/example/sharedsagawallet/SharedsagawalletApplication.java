3aaebef72dc9876172edddc95c63445293bd0dc2
Deepak singh <studydipu@gmail.com>
2026-07-10 16:42:43 +0530

    Added a SHARDING.YML file

---
+++ src/main/java/com/example/sharedsagawallet/SharedsagawalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedsagawallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedsagawalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedsagawalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharedsagawallet/SharedsagawalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharedsagawallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedsagawalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
